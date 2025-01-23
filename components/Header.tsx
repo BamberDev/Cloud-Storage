84826986a43344cb8e2ccadd81ab7fd84225e441
Bamberek <kabaczek99@gmail.com>
2025-01-23 21:33:41 +0100

    added logout dialog component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+"use client";
+
 import { Button } from "./ui/button";
 import Image from "next/image";
 import Search from "./Search";
 import FileUploader from "./FileUploader";
-import { signOutUser } from "@/lib/actions/user.actions";
+import { useState } from "react";
+import LogoutDialog from "./LogoutDialog";
 
 export default function Header({
   $id: ownerId,
@@ -12,27 +14,32 @@ export default function Header({
   $id: string;
   accountId: string;
 }) {
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
+
   return (
     <header className="header">
       <Search />
       <div className="header-wrapper">
         <FileUploader ownerId={ownerId} accountId={accountId} />
-        <form
-          action={async () => {
-            "use server";
-            await signOutUser();
-          }}
-        >
-          <Button type="submit" className="sign-out-button">
-            <Image
-              src="/assets/icons/logout.svg"
-              alt="sign-out"
-              width={24}
-              height={24}
-              className="w-6"
-            />
-          </Button>
-        </form>
+        <LogoutDialog
+          isOpen={isLogoutModalOpen}
+          onOpenChange={setIsLogoutModalOpen}
+          trigger={
+            <Button
+              type="button"
+              className="sign-out-button"
+              onClick={() => setIsLogoutModalOpen(true)}
+            >
+              <Image
+                src="/assets/icons/logout.svg"
+                alt="sign-out"
+                width={24}
+                height={24}
+                className="w-6"
+              />
+            </Button>
+          }
+        />
       </div>
     </header>
   );
