b7f3379383283159f79fa5557edd4a89e53f9734
Bamberek <kabaczek99@gmail.com>
2025-02-08 22:05:04 +0100

    updated logo images and adjust dimensions

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,13 +4,14 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen">
       <section className="hidden w-1/2 items-center justify-center bg-brand p-10 lg:flex xl:w-2/5">
-        <div className="flex flex-col max-h-[800px] max-w-[430px] justify-center items-center space-y-12">
+        <div className="flex flex-col max-h-[800px] max-w-[600px] justify-center items-center space-y-12">
           <Image
-            src="/assets/icons/logo-full.svg"
+            src="/assets/images/logo-white.png"
             alt="logo"
-            width={224}
-            height={82}
-            className="h-auto"
+            width={276}
+            height={100}
+            priority
+            className="h-[70px] w-auto"
           />
 
           <div className="space-y-5 text-white text-center">
@@ -19,8 +20,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <Image
             src="/assets/images/files.png"
             alt="Files"
-            width={342}
-            height={342}
+            width={300}
+            height={300}
             priority
             className="transition-all hover:rotate-2 hover:scale-105"
           />
@@ -30,12 +31,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
         <div className="mb-16 lg:hidden">
           <Image
-            src="/assets/icons/logo-full-brand.svg"
+            src="/assets/images/logo-black.png"
             alt="logo"
-            width={224}
-            height={82}
+            width={276}
+            height={100}
             priority
-            className="h-auto w-[200px] lg:w-[250px]"
+            className="h-[70px] w-auto"
           />
         </div>
 
