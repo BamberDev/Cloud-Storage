ee1fac7ec2c7403878d21256b9e09f43b76d9cf0
Bamberek <kabaczek99@gmail.com>
2025-03-01 03:11:59 +0100

    updated props type

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,21 +1,13 @@
 import { cn, getFileIcon } from "@/lib/utils";
 import Image from "next/image";
 
-interface Props {
-  type: string;
-  extension: string;
-  url?: string;
-  imageClassName?: string;
-  className?: string;
-}
-
 export default function Thumbnail({
   type,
   extension,
   url = "",
   imageClassName,
   className,
-}: Props) {
+}: ThumbnailProps) {
   const isImage = type === "image" && extension !== "svg";
 
   return (
