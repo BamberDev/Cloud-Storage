679d38660fc2650fa4fe8e9cea4ea7719d769c4d
Bamberek <kabaczek99@gmail.com>
2025-03-21 21:54:36 +0100

    refactor cn function to use arrow function syntax

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,9 +3,9 @@ import { clsx, type ClassValue } from "clsx";
 import { Models } from "node-appwrite";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
+export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
-}
+};
 
 export const parseStringify = (value: unknown) => {
   return JSON.parse(JSON.stringify(value));
