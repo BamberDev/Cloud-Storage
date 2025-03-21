a04c91f0fc9f130fc053a37fb11f02e12bb9b760
Bamberek <kabaczek99@gmail.com>
2025-03-21 21:55:28 +0100

    added useErrorToast to simplify error handling and improve toast display

--- hooks/useErrorToast.tsx
+++ hooks/useErrorToast.tsx
@@ -1,43 +1,14 @@
-import { useCallback, useEffect, useRef } from "react";
 import { useToast } from "./use-toast";
 
-export function useErrorToast(hasFileError: boolean, hasSpaceError: boolean) {
+export function useErrorToast() {
   const { toast } = useToast();
-  const toastsShown = useRef(false);
 
-  const showToast = useCallback(() => {
-    if (hasFileError) {
-      toast({
-        description: (
-          <p className="body-2 text-white">
-            Could not load your files. Please try again by refreshing the page.
-          </p>
-        ),
-        className: "error-toast",
-      });
-    }
+  const showErrorToast = (message: string) => {
+    toast({
+      description: <p>{message}</p>,
+      variant: "destructive",
+    });
+  };
 
-    if (hasSpaceError) {
-      toast({
-        description: (
-          <p className="body-2 text-white">
-            Could not load your storage information. Please try again by
-            refreshing the page.
-          </p>
-        ),
-        className: "error-toast",
-      });
-    }
-  }, [hasFileError, hasSpaceError, toast]);
-
-  useEffect(() => {
-    if (!toastsShown.current) {
-      const timer = setTimeout(() => {
-        showToast();
-        toastsShown.current = true;
-      }, 100);
-
-      return () => clearTimeout(timer);
-    }
-  }, [showToast]);
+  return showErrorToast;
 }
