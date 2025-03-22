7d567ce238b3da3a5a9ee61b8135148914d03b93
Bamberek <kabaczek99@gmail.com>
2025-03-22 22:53:20 +0100

    refactor Header component to use SignOutButton and memo for performance improvements

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,46 +1,24 @@
-"use client";
-
-import { Button } from "./ui/button";
-import Image from "next/image";
 import Search from "./Search";
 import FileUploader from "./FileUploader";
-import { useState } from "react";
-import LogoutDialog from "./LogoutDialog";
+import SignOutButton from "./SignOutButton";
+import { memo } from "react";
 
-export default function Header({
+const Header = memo(function Header({
   $id: ownerId,
   accountId,
 }: {
   $id: string;
   accountId: string;
 }) {
-  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
-
   return (
     <header className="header">
       <Search />
       <div className="header-wrapper">
         <FileUploader ownerId={ownerId} accountId={accountId} />
-        <LogoutDialog
-          isOpen={isLogoutModalOpen}
-          onOpenChange={setIsLogoutModalOpen}
-          trigger={
-            <Button
-              type="button"
-              className="sign-out-button"
-              onClick={() => setIsLogoutModalOpen(true)}
-            >
-              <Image
-                src="/assets/icons/logout.png"
-                alt="sign-out"
-                width={64}
-                height={64}
-                className="w-8"
-              />
-            </Button>
-          }
-        />
+        <SignOutButton />
       </div>
     </header>
   );
-}
+});
+
+export default Header;
