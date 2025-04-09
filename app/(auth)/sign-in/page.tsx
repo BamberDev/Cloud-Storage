0dadca7411e08186537877767c5e9d8759bec5eb
Bamberek <kabaczek99@gmail.com>
2025-04-09 03:29:59 +0200

    add generateMetadata to define page title and description

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,5 +1,13 @@
 import AuthForm from "@/components/AuthForm";
 
+export function generateMetadata() {
+  return {
+    title: "Sign In | Cloud Storage",
+    description:
+      "Sign in to your Cloud Storage account to access and manage your files.",
+  };
+}
+
 export default function SignIn() {
   return <AuthForm type="sign-in" />;
 }
