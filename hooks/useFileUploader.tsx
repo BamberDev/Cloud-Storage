e65267c80bc526780e25713b6bbd69f29a8a7665
Bamberek <kabaczek99@gmail.com>
2025-04-23 20:38:02 +0200

    refactor: remove useCallback from uploadFiles function in useFileUploader hook

--- hooks/useFileUploader.tsx
+++ hooks/useFileUploader.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useCallback } from "react";
 import { uploadFile } from "@/lib/actions/file.actions";
 import { useErrorToast } from "./useErrorToast";
 
@@ -12,35 +11,32 @@ export function useFileUploader({
 }: UseFileUploaderProps) {
   const showErrorToast = useErrorToast();
 
-  const uploadFiles = useCallback(
-    async (files: File[]) => {
-      const uploadPromises = files.map(async (file) => {
-        try {
-          const uploadedFile = await uploadFile({
-            file,
-            ownerId,
-            accountId,
-            path,
-          });
-          if (uploadedFile) {
-            onFileProcessed(file.name);
-          }
-        } catch (error) {
-          if (
-            error instanceof Error &&
-            error.message === "Storage limit reached."
-          ) {
-            showErrorToast("Storage limit reached. Unable to upload file.");
-          } else {
-            showErrorToast("Failed to upload file. Please try again.");
-          }
+  const uploadFiles = async (files: File[]) => {
+    const uploadPromises = files.map(async (file) => {
+      try {
+        const uploadedFile = await uploadFile({
+          file,
+          ownerId,
+          accountId,
+          path,
+        });
+        if (uploadedFile) {
           onFileProcessed(file.name);
         }
-      });
-      await Promise.all(uploadPromises);
-    },
-    [ownerId, accountId, path, onFileProcessed, showErrorToast]
-  );
+      } catch (error) {
+        if (
+          error instanceof Error &&
+          error.message === "Storage limit reached."
+        ) {
+          showErrorToast("Storage limit reached. Unable to upload file.");
+        } else {
+          showErrorToast("Failed to upload file. Please try again.");
+        }
+        onFileProcessed(file.name);
+      }
+    });
+    await Promise.all(uploadPromises);
+  };
 
   return { uploadFiles };
 }
